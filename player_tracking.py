549d599b94a20ae6fa5b8cedf6c5f451e0716476
Dmitriy <dimon.grem@yandex.ru>
2016-11-22 23:38:11 +0300

    almost completed player search

--- player_tracking.py
+++ player_tracking.py
@@ -1,69 +1,122 @@
 import cv2
 import numpy as np
-from matplotlib import pyplot as plt
+import math
+
+def dist(x,y,X,Y):
+    return math.sqrt((x-X)**2 + (y-Y)**2)
 
 cap = cv2.VideoCapture('Main2.mp4')
-cap.set(cv2.CAP_PROP_POS_MSEC,470000)
+cap.set(cv2.CAP_PROP_POS_MSEC,450000)
 back = cv2.imread('back.jpg', 0)
 back2 = cv2.imread('frame21.jpg', 0)
 
+paused = False
+x, y = -1, -1
+
 while True:
-    ret, frame = cap.read()
     
-    if ret == True:
+    if not paused:
         
-        # setting a ROI
-        mask = np.zeros(frame.shape, dtype=np.uint8)
-        roi_corners = np.array([[(0,720),(0,460),(440,280),(880,280),(1280,460),(1280,720)]], dtype=np.int32)
-        channel_count = frame.shape[2]  # i.e. 3 or 4 depending on your img
-        ignore_mask_color = (255,)*channel_count
-        cv2.fillPoly(mask, roi_corners, ignore_mask_color)
-        frame = cv2.bitwise_and(frame, mask)
+        ret, frame = cap.read()
         
-        frame = cv2.cvtColor(frame, cv2.COLOR_BGR2GRAY)
-        
-        # background subtraction
-        diff = cv2.subtract(frame, back2)
-        
-        _, thresh = cv2.threshold(diff,10,255, 0)
-        
-        # contours search
-        _, cont, hier = cv2.findContours(thresh, cv2.RETR_LIST, cv2.CHAIN_APPROX_SIMPLE)
+        if ret == True:
+            
+            if not paused:
+                
+                # setting a ROI
+                mask = np.zeros(frame.shape, dtype=np.uint8)
+                roi_corners = np.array([[(0,720),(0,460),(440,280),(880,280),(1280,460),(1280,720)]], dtype=np.int32)
+                channel_count = frame.shape[2]  # i.e. 3 or 4 depending on your img
+                ignore_mask_color = (255,)*channel_count
+                cv2.fillPoly(mask, roi_corners, ignore_mask_color)
+                Frame = cv2.bitwise_and(frame, mask)
+                
+                Frame = cv2.cvtColor(Frame, cv2.COLOR_BGR2GRAY)
+                
+                # background subtraction
+                diff = cv2.subtract(Frame, back2)
+                
+                _, thresh = cv2.threshold(diff, 30, 255, cv2.THRESH_BINARY)
+                kernel = np.ones((2, 2), dtype = np.uint8)
+                thresh = cv2.erode(thresh, kernel, iterations=4)
+                thresh = cv2.dilate(thresh, kernel, iterations=4)
+                _, cont, hier = cv2.findContours(thresh, cv2.RETR_LIST, cv2.CHAIN_APPROX_SIMPLE)
 
-        # searching for the maximal area contour
-        # I am sure that it is player's body
-        maxArea = 0
-        maxCnt = []
-        for elem in cont:
-            area = cv2.contourArea(elem)
-            if(area>maxArea):
-                maxArea = area
-                cnt = elem
-        maxCnt.append(cnt)
+                # ищем контур с максимальной площадью
+                # смею предположить, что это туловище игрока
+                maxArea = 0
+                for elem in cont:
+                    area = cv2.contourArea(elem)
+                    if area > maxArea:
+                        maxArea = area
+                        maxCnt = elem
+                        
+                # ищем центр масс тела
+                M = cv2.moments(maxCnt)
+                Cx = int(M['m10']/M['m00'])
+                Cy = int(M['m01']/M['m00'])
 
-        # adding such elements as rocket and feet to contours array
-        manCnt = []
-        for elem in cont:
-            if cv2.contourArea(elem) > maxArea * 0.03:
-                manCnt.append(elem)
-        cv2.drawContours(thresh, manCnt, -1, 200, -1)
-        
-        # polygonal approximation of player's contour
-        approxCnt = []        
-        for elem in manCnt:
-            epsilon = 0.01*cv2.arcLength(elem,True)
-            approx = cv2.approxPolyDP(elem,epsilon,True)
-            approxCnt.append(approx) 
-        cv2.drawContours(thresh, approxCnt, -1, 50, -1)
-        
-        cv2.imshow('diff', diff)
-        cv2.imshow('thresh', thresh)
-  
-        if cv2.waitKey(25) & 0xFF == 27:
-            break
-        
-    else:
-        print('ACHTUNG!')
+                # предполагаемый контур игрока
+                manCnt = []
+                
+                # выбираем контуры, находящиеся неподалёку от контура туловища
+                for elem in cont:
+                    Mm = cv2.moments(elem)
+                    if Mm['m00'] == 0:
+                        continue
+                    ccx = int(Mm['m10']/Mm['m00'])
+                    ccy = int(Mm['m01']/Mm['m00'])
+                    
+                    # убираем элементы с непропорционально большими/малыми высотами, т.е. линии
+                    _,_,w,h = cv2.boundingRect(elem)
+                    if 0.5 < w/h < 3:
+                        if dist(Cx, Cy, ccx, ccy) <= 200:
+                            manCnt.append(elem)
+                Cont = cv2.drawContours(thresh, manCnt, -1, 60, -1)
+                
+                # находим два самых нижних контура среди различных контуров игрока
+                minx1, miny1, minx2, miny2 = 0, 0, 0, 0
+                for cont in manCnt:
+                    M = cv2.moments(cont)
+                    cx = int(M['m10']/M['m00'])
+                    cy = int(M['m01']/M['m00'])
+                    if cy > miny1:
+                        minx2, miny2 = minx1, minx2
+                        minx1, miny1 = cx, cy
+                    elif cy > miny2:
+                        minx2, miny2 = cx, cy
+                        
+                # обработка случая, когда одна нога находится за другой
+                if abs(minx1-minx2) < 10:
+                    minx2 = minx1
+                    miny2 = miny1
+                    cv2.putText(frame, '!!!', (80, 80), cv2.FONT_HERSHEY_SIMPLEX, 2, (0, 0, 255), 5)
+                
+                # выводим результаты в окно с контурами
+                Thresh = cv2.cvtColor(thresh, cv2.COLOR_GRAY2BGR)
+                cv2.circle(Thresh, (Cx, Cy), 5, (0, 150, 0), -1)
+                cv2.circle(Thresh, (minx1, miny1), 5, (0, 0, 255), -1)
+                cv2.circle(Thresh, (minx2, miny2), 5, (0, 0, 255), -1)
+                cv2.line(Thresh, (Cx, Cy), (minx1, miny1), (255, 0, 0), 2)
+                cv2.line(Thresh, (Cx, Cy), (minx2, miny2), (255, 0, 0), 2)
+                
+                # выводим результаты в окно с исходной картинкой
+                cv2.circle(frame, (minx1, miny1), 5, (0, 0, 255), -1)
+                cv2.circle(frame, (minx2, miny2), 5, (0, 0, 255), -1)
+                cv2.circle(frame, (Cx, Cy), 5, (0, 150, 0), -1)
+                cv2.line(frame, (Cx, Cy), (minx1, miny1), (255, 0, 0), 2)
+                cv2.line(frame, (Cx, Cy), (minx2, miny2), (255, 0, 0), 2)
+
+    cv2.imshow('diff', diff)
+    cv2.imshow('Cont', Cont)
+    cv2.imshow('thresh', Thresh)
+    cv2.imshow('frame', frame)
+
+    k = cv2.waitKey(2)
+
+    if k & 0xFF == ord(' '):
+        paused = not paused
+    elif k & 0xFF == 27:
         break
 
 cap.release()
--- test.py
+++ test.py
@@ -1,36 +1,136 @@
 import cv2
 import numpy as np
+from matplotlib import pyplot as plt
 
-back = cv2.imread('frame21.jpg', 0)
 cap = cv2.VideoCapture('Main2.mp4')
-cap.set(cv2.CAP_PROP_POS_MSEC,470000)
+cap.set(cv2.CAP_PROP_POS_MSEC, 450000)
+back = cv2.imread('back.jpg', 0)
+back2 = cv2.createBackgroundSubtractorMOG2(history=50, detectShadows=0)
+ballfound = False
+xprev=10000
+yprev=10000
 
 while True:
+    ret, frame = cap.read()
     
+    if ret == True:
+        
+        # setting a ROI
+        mask = np.zeros(frame.shape, dtype=np.uint8)
+        roi_corners = np.array([[(0,720),(0,460),(480,230),(800,230),(1280,460),(1280,720)]], dtype=np.int32)
+        channel_count = frame.shape[2]  # i.e. 3 or 4 depending on your img
+        ignore_mask_color = (255,)*channel_count
+        cv2.fillPoly(mask, roi_corners, ignore_mask_color)
+        frame = cv2.bitwise_and(frame, mask)
+        
+        frame = cv2.cvtColor(frame, cv2.COLOR_BGR2GRAY)
+        
+        # background subtraction block
+#        diff = cv2.subtract(frame, back)
+        diff = back2.apply(frame)
+        _, diff = cv2.threshold(diff,5,255, cv2.THRESH_BINARY_INV)
+        
+        
+        # doubtful part
+        #kernel = np.ones((2,2),np.uint8)
+        #diff = cv2.dilate(diff,kernel,iterations = 2)
+        #diff = cv2.erode(diff,kernel,iterations = 2)
+        
+        cv2.imshow('diff', diff)
+        
+        params = cv2.SimpleBlobDetector_Params()
 
-    _, frame = cap.read()
+        params.filterByConvexity = True
+        params.minConvexity = 0.8
     
-    mask = np.zeros(frame.shape, dtype=np.uint8)
-    roi_corners = np.array([[(0,720),(0,460),(440,280),(880,280),(1280,460),(1280,720)]], dtype=np.int32)
-    channel_count = frame.shape[2]  # i.e. 3 or 4 depending on your img
-    ignore_mask_color = (255,)*channel_count
-    cv2.fillPoly(mask, roi_corners, ignore_mask_color)
-    
-    frame = cv2.bitwise_and(frame, mask)
-    
-    frame = cv2.cvtColor(frame, cv2.COLOR_BGR2GRAY)
-    
-    diff = cv2.subtract(frame, back)
-    
-    _, thresh = cv2.threshold(diff, 10, 255, 0)
-    
-    diff = cv2.bitwise_and(diff, diff, mask = thresh)
-    
-#    _, _, _ = cv2.findContours(diff, cv2.RETR_TREE, cv2.CHAIN_APPROX_SIMPLE)
-    cv2.imshow('diff', diff)
-    cv2.imshow('thresh', thresh)
-    
-    if cv2.waitKey(25) & 0xFF == 27:
+        params.filterByInertia = True
+        params.minInertiaRatio = 0.3
+        
+        params.filterByArea = True
+        params.minArea = 15
+        params.maxArea = 300
+ 
+        params.filterByCircularity = True
+        params.minCircularity = 0.3
+
+
+        detector = cv2.SimpleBlobDetector_create(params)
+
+        # Detect blobs.
+        keypoints = detector.detect(diff)
+         
+        # Draw detected blobs as red circles.
+        # cv2.DRAW_MATCHES_FLAGS_DRAW_RICH_KEYPOINTS ensures the size of the circle corresponds to the size of blob
+
+	# сделаем цветастенько
+        frame = cv2.cvtColor(frame, cv2.COLOR_GRAY2BGR)
+        
+        
+        
+        
+#        # shit begins
+#        
+        # проверяем, видим ли мы мячик в окрестности его последнего расположения
+        ballfound = False
+        for elem in keypoints:
+            if ((abs(elem.pt[0]-xprev)<50) or (abs(elem.pt[1]-yprev)<50)):
+                ballfound = True
+        
+        
+        # видим
+	# находим самый близкий к предыдущей дислокации мячика блоб и нарекаем его мячиком
+        xmin=9000
+        ymin=9000
+        if ballfound:
+            for elem in keypoints:
+                if ((abs(elem.pt[0]-xprev)<xmin)&(abs(elem.pt[1]-yprev)<ymin)):
+                    xmin = abs(elem.pt[0]-xprev)
+                    ymin = abs(elem.pt[1]-yprev)
+                    xcur=elem.pt[0]
+                    ycur=elem.pt[1]
+            cv2.circle(frame, (int(xcur), int(ycur)), 5, (33, 255, 237), 2) 
+            cv2.putText(frame, 'BALL', (int(xcur)+10, int(ycur)+10), cv2.FONT_HERSHEY_SIMPLEX, 0.5, (33, 255, 237), 1)
+            xprev=xcur
+            yprev=ycur
+           
+	# не видим
+	# тут я не смог придумать, как найти площадь блоба по кейпоинту, поэтому мячом нарекается последний контур из кейпоинтов
+	# эту ересь надо починить 
+        if not ballfound:
+            for elem in keypoints:
+                ballfound = True
+                xprev=elem.pt[0]
+                yprev=elem.pt[1]
+                cv2.circle(frame, (int(elem.pt[0]), int(elem.pt[1])), 5, (33, 255, 237), 2) 
+                cv2.putText(frame, 'BALL', (int(elem.pt[0])+10, int(elem.pt[1])+10), cv2.FONT_HERSHEY_SIMPLEX, 0.5, (33, 255, 237), 1) 
+        
+        
+        
+        
+#        # shit ends
+        
+        
+        
+        # это не нужно, это вывод мяча без всей той телеги выше.
+#        for elem in keypoints:
+#            x=elem.pt[0]
+#            y=elem.pt[1]
+#            cv2.circle(frame, (int(x), int(y)), 5, (33, 255, 237), 2) 
+#            cv2.putText(frame, 'BALL', (int(x)+10, int(y)+10), cv2.FONT_HERSHEY_SIMPLEX, 0.5, (33, 255, 237), 1) 
+        
+        # почти то же самое, что и пять строк выше
+        #frame = cv2.drawKeypoints(frame, keypoints, np.array([]), (0,0,255), cv2.DRAW_MATCHES_FLAGS_DRAW_RICH_KEYPOINTS)
+         
+        # Show keypoints
+        cv2.imshow("Keypoints", frame)
+        
+        
+        if cv2.waitKey(25) & 0xFF == 27:
+            break
+        
+    else:
+        print('ACHTUNG!')
         break
 
+cap.release()
 cv2.destroyAllWindows()
